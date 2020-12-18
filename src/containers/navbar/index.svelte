729406e372d5a61bb60da965fe4e311a2bb545e8
anidast <anidwi.ad1@gmail.com>
2020-12-18 09:46:04 +0700

    image

--- src/containers/navbar/index.svelte
+++ src/containers/navbar/index.svelte
@@ -34,7 +34,7 @@
 <nav class="navbar is-transparent is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand py-4">
 	  <a class="navbar-item has-text-black is-uppercase is-size-4 mx-5" href="/">
-		<img src="logo.png" alt="logo AEKI">
+		<img src="image/logo.png" alt="logo AEKI">
 	  </a>
 	  <a
 	  role="button"
