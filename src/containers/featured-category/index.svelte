dbeb67cfdf446168f54ef561a0d8a9de6f233ebf
anidast <anidwi.ad1@gmail.com>
2020-12-19 13:30:36 +0700

    featured category

---
+++ src/containers/featured-category/index.svelte
@@ -0,0 +1,82 @@
+<script>
+    import "./index.scss";
+</script>
+
+<section class="section">
+    <p class="title is-size-3 has-text-weight-bold has-text-centered"> h2 Featured Category</p>
+    
+    <div class="columns is-multiline">
+        <div class="column is-three-fifths has-background-light p-6 my-4">
+            <div class="columns">
+                <div class="column is-half">
+                    <p class="subtitle">Premium Quality</p>
+                    <p class="title">Latest Dining Table</p>
+                    <br><br>
+                    <a href="/" class="has-text-warning has-text-weight-bold is-uppercase">
+                        Explor Now <i class="fa fa-caret-right" />
+                    </a>
+                </div>
+                <div class="column is-half">
+                    <figure class="image is-square">
+                        <img src="image/table.png" alt="table">
+                    </figure>
+                </div>
+            </div>
+        </div>
+
+        <div class="column is-two-fifths p-6 my-4">
+            <div class="columns">
+                <div class="column is-half">
+                    <p class="subtitle">Premium Quality</p>
+                    <p class="title">Latest Light Lamp</p>
+                    <br><br>
+                    <a href="/" class="has-text-warning has-text-weight-bold is-uppercase">
+                        Explor Now <i class="fa fa-caret-right" />
+                    </a>
+                </div>
+                <div class="column is-half">
+                    <figure class="image is-4by3">
+                        <img src="image/lamp.png" alt="lamp">
+                    </figure>
+                </div>
+            </div>
+        </div>
+
+        <div class="column is-two-fifths p-6 my-4">
+            <div class="columns">
+                <div class="column is-half">
+                    <p class="subtitle">Premium Quality</p>
+                    <p class="title">Latest Chair Desk</p>
+                    <br><br>
+                    <a href="/" class="has-text-warning has-text-weight-bold is-uppercase">
+                        Explor Now <i class="fa fa-caret-right" />
+                    </a>
+                </div>
+                <div class="column is-half">
+                    <figure class="image is-2by3">
+                        <img src="image/chair.png" alt="chair">
+                    </figure>
+                </div>
+            </div>
+        </div>
+
+        <div class="column is-three-fifths has-background-light p-6 my-4">
+            <div class="columns">
+                <div class="column is-one-third">
+                    <p class="subtitle">Premium Quality</p>
+                    <p class="title">Latest Foam Sofa</p>
+                    <br><br>
+                    <a href="/" class="has-text-warning has-text-weight-bold is-uppercase">
+                        Explor Now <i class="fa fa-caret-right" />
+                    </a>
+                </div>
+                <div class="column is-two-third">
+                    <figure class="image is-3by1">
+                        <img src="image/sofa.png" alt="sofa">
+                    </figure>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
