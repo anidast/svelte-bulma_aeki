50ab37772b70b8001285d44a282d60809540bb1b
galuhnr <galuhnurul99@gmail.com>
2020-12-25 17:55:18 +0700

    product slider, list

--- src/containers/products-slider/index.svelte
+++ src/containers/products-slider/index.svelte
@@ -9,7 +9,7 @@
         <div class="column">
             <div class="product_item">
                 <div class="card-image">
-                  <figure class="image is-4by5">
+                  <figure class="image is-4by4">
                     <img src="image/chair2.png" alt="chair">
                   </figure>
                 </div>
@@ -27,7 +27,7 @@
         <div class="column">
           <div class="product_item">
             <div class="card-image">
-              <figure class="image is-4by5">
+              <figure class="image is-4by4">
                 <img src="image/chair2.png" alt="chair">
               </figure>
             </div>
@@ -42,7 +42,41 @@
             </div>
           </div>
         </div>
-        <div class="column"></div>
-        <div class="column"></div>
+        <div class="column">
+          <div class="product_item">
+            <div class="card-image">
+              <figure class="image is-4by4">
+                <img src="image/chair2.png" alt="chair">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content product_text">
+                <p class="title is-size-5 has-text-weight-bold mb-5">Quart Bel Chair</p>
+                <p class="subtitle">Rp. 345.000</p>
+                <p class="text-chart">
+                  <a href="/">+ ADD TO CHART</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="product_item">
+            <div class="card-image">
+              <figure class="image is-4by4">
+                <img src="image/chair2.png" alt="chair">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content product_text">
+                <p class="title is-size-5 has-text-weight-bold mb-5">Quart Bel Chair</p>
+                <p class="subtitle">Rp. 345.000</p>
+                <p class="text-chart">
+                  <a href="/">+ ADD TO CHART</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
 </section>
