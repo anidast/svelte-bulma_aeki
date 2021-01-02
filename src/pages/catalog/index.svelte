2a1389ec1403b415e077a40e04a285c180e3851b
anidast <anidwi.ad1@gmail.com>
2021-01-02 20:03:22 +0700

    revisi

--- src/pages/catalog/index.svelte
+++ src/pages/catalog/index.svelte
@@ -5,61 +5,6 @@
 
     export let category;
 
-    //dummy
-    // let products = [
-    //     {
-    //         id: 1,
-    //         product_name: "Quart Bel Chair",
-    //         product_price: 345000,
-    //         product_photos: [
-    //             {
-    //                 photo_url: "http://localhost:5000/image/chair2.png",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 1,
-    //         product_name: "Quart Bel Chair",
-    //         product_price: 345000,
-    //         product_photos: [
-    //             {
-    //                 photo_url: "http://localhost:5000/image/chair2.png",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 1,
-    //         product_name: "Quart Bel Chair",
-    //         product_price: 345000,
-    //         product_photos: [
-    //             {
-    //                 photo_url: "http://localhost:5000/image/chair2.png",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 1,
-    //         product_name: "Quart Bel Chair",
-    //         product_price: 345000,
-    //         product_photos: [
-    //             {
-    //                 photo_url: "http://localhost:5000/image/chair2.png",
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         id: 1,
-    //         product_name: "Quart Bel Chair",
-    //         product_price: 345000,
-    //         product_photos: [
-    //             {
-    //                 photo_url: "http://localhost:5000/image/chair2.png",
-    //             },
-    //         ],
-    //     },
-    // ];
-    //end of dummy
-
     let products;
     const getproducts = (async () => {
         const response = await fetch(apiUrl + "product/category/" + category, {
--- src/pages/product/index.svelte
+++ src/pages/product/index.svelte
@@ -8,106 +8,19 @@
     } from "../../containers";
     import { apiUrl } from "../../stores";
 
-    //dummy
-    let product = {
-        product_name: "kursi",
-        product_description: "sebuah kursi",
-        product_price: 120500,
-        product_vendor: "Mebel International",
-        product_category: "chair",
-        product_durability: "Tahan 10Kg",
-        product_width: 60,
-        product_color: "Merah",
-        product_material: "plastic",
-        product_height: 120,
-        product_photos: [
-            {
-                photo_url: "http://localhost:5000/image/chair2.png",
-            },
-            {
-                photo_url: "http://localhost:5000/image/chair2.png",
-            },
-            {
-                photo_url: "http://localhost:5000/image/chair2.png",
-            },
-            {
-                photo_url: "http://localhost:5000/image/chair2.png",
-            },
-        ],
-        id: 12,
-    };
-
-    let products = [
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-        {
-            product_name: "Quart Bel Chair",
-            product_price: 345000,
-            product_photos: [
-                {
-                    photo_url: "http://localhost:5000/image/chair2.png",
-                },
-            ],
-        },
-    ];
-    //end of dummy
-
     export let id;
-    // let product = {};
-    // let products = [];
+    let product = {};
+    let products = [];
 
-    // const getproduct = (async () => {
-    //     const response = await fetch(apiUrl + "product/" + id, {
-    //         method: "GET",
-    //     });
-    //     let res = await response.json();
-    //     if (response.status == 200) {
-    //         product = res.data;
-    //     }
-    // })();
+    const getproduct = (async () => {
+        const response = await fetch(apiUrl + "product/" + id, {
+            method: "GET",
+        });
+        let res = await response.json();
+        if (response.status == 200) {
+            product = res.data;
+        }
+    })();
 
     // const getproducts = (async () => {
     //     const response = await fetch(apiUrl + "product/category/" + product.product_category, {
